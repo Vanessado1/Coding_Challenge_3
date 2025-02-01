c1717956c673cd340f12382cbc2ae8c7541dbbe7
Vanessa Do <xex.vanessa@gmail.com>
2025-02-01 14:28:39 -0500

    Task 1 - Product Price Management

---
+++ cc_3.js
@@ -0,0 +1,5 @@
+// Task 1- Price Management Scenario 
+let price = [5, 12, 9, 20, 19]; //Declared Price Array
+console.log("Original",price);
+price.splice(0,1,26);//Removed 5 and added 26
+console.log("Updated Price",price); // Updated Price Array 
